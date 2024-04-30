c06a51a13b89a58b63e9682544c227e9e68b4741
IT-Academy <ashabttembul@gmail.com>
2024-04-30 19:22:17 +0600

    ASASA

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index),
-    path('rare/',views.main)
+
 ]
--- main/views.py
+++ main/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    users = ["Admin", "User", "Guest"]
-    return render(request, 'register.html', {'users': users})
-
-
+    return render(request, 'main.html')
