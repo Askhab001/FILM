f1dc04a65541c11297bbb265e8c107a257af360e
ashab <ashabttembul@gmail.com>
2024-05-02 13:14:49 +0600

    MRX

--- main/views.py
+++ main/views.py
@@ -2,12 +2,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'register.html')
-
-
-def main(request):
-    return render(request, 'assa.html')
-
-
-def aza(request):
     return render(request, 'aza.html')
+
